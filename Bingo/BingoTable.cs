1c3adbe60dee62ada8553cecb41219ac4dc328e6
sarawut positwinyu <sarawut.positwinyu@gmail.com>
2018-08-22 15:10:20 +0700

    add project

---
+++ Bingo/BingoTable.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo
+{
+    public class BingoTable
+    {
+        public const int TABLE_SIZE = 5;
+
+        public bool IsBingo(IEnumerable<int> inputs)
+        {
+            return this.isSameColumnBingo(inputs) ||
+                   this.isSameRowBingo(inputs) ||
+                   this.isLeftDiagonalBingo(inputs) ||
+                   this.isRightDiagonalBingo(inputs);                   
+        }
+
+        private bool isSameColumnBingo(IEnumerable<int> inputs)
+        {
+            var groupedByColumnIndex = inputs.GroupBy(input => input % TABLE_SIZE);
+            var anyFullColumn = groupedByColumnIndex.Any(grouped => grouped.Count() == TABLE_SIZE);
+            return anyFullColumn;
+        }
+
+        private bool isSameRowBingo(IEnumerable<int> inputs)
+        {
+            var groupedByRowIndex = inputs.GroupBy(input => Math.Ceiling( (double)input / (double)TABLE_SIZE));
+            var anyFullRow = groupedByRowIndex.Any(grouped => grouped.Count() == TABLE_SIZE);
+            return anyFullRow;
+        }
+
+        
+        private IEnumerable<int> getLeftDiagonalBingo()
+        {
+            return Enumerable.Range(0, TABLE_SIZE)
+                             .Select((index) => 1 + ( (TABLE_SIZE + 1) * index));
+        }
+
+        private bool isLeftDiagonalBingo(IEnumerable<int> inputs)
+        {
+            return getLeftDiagonalBingo().All(item => inputs.Contains(item));
+        }
+
+        private IEnumerable<int> getRightDiagonalBingo()
+        {
+            return Enumerable.Range(0, TABLE_SIZE)
+                             .Select((index) => TABLE_SIZE + ((TABLE_SIZE + -1) * index));
+        }
+
+        private bool isRightDiagonalBingo(IEnumerable<int> inputs)
+        {
+            return getRightDiagonalBingo().All(item => inputs.Contains(item));
+        }
+    }
+}
---
+++ Bingo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BingoTable bingoTable = new BingoTable();
+
+            Console.WriteLine(bingoTable.IsBingo(new int[] { 3,4,8,13,18,19,23}));
+            Console.WriteLine(bingoTable.IsBingo(new int[] { 1,13,19,25,23,2}));
+            Console.WriteLine(bingoTable.IsBingo(new int[] { 2,1,12,15,6,18,16,4,3,21,11 }));
+        }
+    }
+}
